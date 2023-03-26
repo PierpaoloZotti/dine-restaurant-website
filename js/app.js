6a230bcf59c94dfb25f690500cc836a01b7cbf70
rweventos <rweventos.rj@gmail.com>
2023-03-26 20:02:13 -0300

    minor changes

--- js/app.js
+++ js/app.js
@@ -20,3 +20,31 @@ function showFamily(){
     container[0].className="container showFamily";
     family.style.opacity=1;
 };
+
+function labelAnimation(id){
+    const element = document.getElementById(id+'_label');
+    element[0].className="labelAnimation";
+    
+}
+
+$('[data-option]').on('click', function(event){
+    var optionId = event.target.id;
+    const element = document.getElementById(optionId+'_label');
+    element.className="labelAnimation";
+    console.log(element);
+})
+
+const labels = document.querySelectorAll(".form_control label");
+
+labels.forEach((label) => {
+  label.innerHTML = label.innerHTML
+
+    .split("")
+
+    .map(
+      (letter, idx) =>
+        `<span style="transition-delay:${idx * 50}ms">${letter}</span>`
+    )
+
+    .join("");
+});
